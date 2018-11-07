20655df236e7dd0c423c01da47558031e1729169
nick12340 <37206316+nick12340@users.noreply.github.com>
2018-11-07 11:56:26 +0800

    Update perceptron.py

--- perceptron.py
+++ perceptron.py
@@ -1,55 +1,49 @@
-import math
-
-def sigmoid(x):
-  return 1 / (1 + math.exp(-x))
-
-class perceptron(object):
+class Perceptron:
     def __init__(self,input_num,activator):
         self.activator = activator
-        self.weights = [0.0 for _ in range(input_num)]
+        self.weights = [0.0] * input_num
         self.bias = 0.0
+
     def __str__(self):
         return 'weights\t:%s\nbias\t:%f\n' % (self.weights,self.bias)
-
+    
     def predict(self,input_vec):
-        pair = zip(input_vec , self.weights)
-        multipy = map(lambda (a,b): a*b, pair)
-        def add(x,y): return x + y
-        sum = reduce(add, multipy,0.0)
-        return self.activator (sum +self.bias)
-
-    def train(self,input_vecs,labels,iteration,rate):
-        for i in range(iteration):
-            self.iterate(input_vecs,labels,rate)
-
-    def update_weight(self,input_vec,output,label,rate):
+        weighted_input = map(lambda w,x:w*x,self.weights,input_vec)
+        #print (list(weighted_input))
+        return (self.activator(sum(weighted_input)+self.bias))
+    
+    def update_weights(self,input_vec,output,label,rate):
         delta = label - output
-        self.weights =map(lambda (x,w): w + rate * delta * x,zip(input_vec,self.weights))
-        self.bias = self.bias + rate*delta
+        self.weights = list(map(lambda w,x: w+rate*delta*x,self.weights,input_vec))
+        self.bias = self.bias + rate * delta
 
-    def iterate(self,input_vecs,labels,rate):
-        samples = zip(input_vecs,labels)
-        for (input_vec,label) in samples:
+    def iterate(self,input_vecs,label,rate):
+        sample = zip(input_vecs,label)
+        for input_vec,label in sample:
             output = self.predict(input_vec)
-            self.update_weight(input_vec,output,label,rate)
+            self.update_weights(input_vec,output,label,rate)
+
+    def train(self,input_vecs,label,iteration,rate):
+        for i in range(iteration):
+            self.iterate(input_vecs,label,rate)
 
 def f(x):
-    return 1 if x > 0 else 0
-
-def get_data():
+    return 1 if x>0 else 0
+def get_train_data():
     input_vecs = [[1,1],[0,0],[1,0],[0,1]]
     labels = [1,0,0,0]
     return input_vecs,labels
-def train_perceptron():
-    p = perceptron(2,f)
-    input_vecs,labels = get_data()
+
+def train():
+    p = Perceptron(2,f)
+    input_vecs,labels = get_train_data()
     p.train(input_vecs,labels,10,0.1)
     return p
 
 if __name__ == '__main__':
-    sb = train_perceptron()
-    print sb
-    print '1 and 1 = %d' % sb.predict([1, 1])
-    print '0 and 0 = %d' % sb.predict([0, 0])
-    print '1 and 0 = %d' % sb.predict([1, 0])
-    print '0 and 1 = %d' % sb.predict([0, 1])
+    p = train()
+    print (p)
+    print ('1 and 1 = %d' % p.predict([1, 1]))
+    print ('0 and 0 = %d' % p.predict([0, 0]))
+    print ('1 and 0 = %d' % p.predict([1, 0]))
+    print ('0 and 1 = %d' % p.predict([0, 1]))
